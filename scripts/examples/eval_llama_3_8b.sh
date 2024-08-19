8567abfd769e89cb406e86e674ee8878e4839db4
floatingbigcat <qisun@sakana.ai>
2024-08-19 17:50:52 +0900

    get example script

---
+++ scripts/examples/eval_llama_3_8b.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+save_dir="eval_results/"
+global_record_file="eval_results/eval_record_collection.csv"
+model="meta-llama/Meta-Llama-3-8B-Instruct"
+selected_subjects="all"
+gpu_util=0.8
+
+cd ../../
+export CUDA_VISIBLE_DEVICES=0
+
+python evaluate_from_local.py \
+                 -k 0 \
+                 --selected_subjects $selected_subjects \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util
+
